e7f00d53330c922ecb3cfd5d69760fbf0beac1d0
Loay Khateeb <loay.106@gmail.com>
2024-12-31 18:28:07 +0200

    fix minor bug

--- SurfboardControlUnit/src/Time/TimeManager.cpp
+++ SurfboardControlUnit/src/Time/TimeManager.cpp
@@ -13,7 +13,7 @@ void TimeManager::initialize()
 
 int TimeManager::getCurrentTimestamp(){
     DateTime time = rtc.now();
-    return time.timestamp(DateTime::TIMESTAMP_FULL))
+    return time.timestamp(DateTime::TIMESTAMP_FULL);
 }
 
 void TimeManager::adjust(DateTime current){
