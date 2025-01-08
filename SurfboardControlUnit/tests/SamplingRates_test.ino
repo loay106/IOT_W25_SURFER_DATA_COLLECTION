8d1f91b6b1e7ee9ad45826573428abeff750571c
Loay Khateeb <loay.106@gmail.com>
2025-01-08 17:45:01 +0200

    update test

--- SurfboardControlUnit/tests/SamplingRates_test.ino
+++ SurfboardControlUnit/tests/SamplingRates_test.ino
@@ -7,20 +7,20 @@ using namespace std;
 
 uint8_t CONTROL_UNIT_MAC[] = {0xA8, 0x42, 0xE3, 0x46, 0xE0, 0x64};
 
-const char* IMU_SAMPLE_MOCK = "SAMPLE_SYNC|1547|AccX AccY AccZ|0.23 -9.77 -1.67|0.23 -10.77 -1.57|1.23 -9.78 -1.57|7.23 -9.77 -1.57|7.23 -9.77 -1.57|7.23 -9.77 -1.57|7.23 -9.77 -1.57|7.23 -9.77 -1.57";
+const char* IMU_SAMPLE_MOCK = "SAMPLE_SYNC|1547|AccX AccY AccZ|0.23 -9.77 -1.67|0.23 -10.77 -1.57|1.23 -9.78 -1.57|7.23 -9.77 -1.57|7.23 -9.77 -1.57|7.23 -9.77 -1.57|7.23 -9.77 -1.57|7.23 -9.77 -1.57|7.23 -9.77 -1.57";
 const char* STRAIN_GAUGE_SAMPLE_MOCK = "SAMPLE_SYNC|1447|N|5456|5421|4242|4240|7540|7040|4812|3478|7484";
 const char* STAND_BY_STATUS = "STATUS_UPDATE|STAND_BY";
 const char* SAMPLING_STATUS = "STATUS_UPDATE|SAMPLING";
 const char* sensorType = "IMU";
 
-int MAX_SAMPLES_PER_SECOND = 30;
+int MAX_SAMPLES_PER_SECOND = 100;
 int samplingStartTime = 0;
 int samplesSentCount = 0;
 string COMMAND;
 
 void onCommandReceived(const uint8_t* mac_addr, const uint8_t* incomingData, int len) {
     string message(reinterpret_cast<const char*>(incomingData), len);
-    Serial.println(message.c_str());
+    // Serial.println(message.c_str());
     if (message.find("START_SAMPLING") == 0) {
         COMMAND = string("START_SAMPLING");
         Serial.println("Sampling started...");
@@ -37,6 +37,10 @@ void onCommandReceived(const uint8_t* mac_addr, const uint8_t* incomingData, int
         int elapsedSeconds = (millis() - samplingStartTime) / 1000;
         int sendRate = elapsedSeconds > 0 ? samplesSentCount / elapsedSeconds : 0;
         Serial.printf("Send rate: %d Hz\n", sendRate);
+        esp_err_t result = esp_now_send(CONTROL_UNIT_MAC, (uint8_t *) STAND_BY_STATUS, strlen(STAND_BY_STATUS));
+        if (result != ESP_OK) {
+            Serial.println("Failed to send SAMPLING status");
+        }
     }
 }
 
