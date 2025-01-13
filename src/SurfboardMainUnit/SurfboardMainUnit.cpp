c5dfba752180673a517fdcec1b4322639afce5d6
Loay Khateeb <loay.106@gmail.com>
2025-01-13 23:22:10 +0200

    merge Controller with main unit + update the handling of internal sampler

--- src/SurfboardMainUnit/SurfboardMainUnit.cpp
+++ src/SurfboardMainUnit/SurfboardMainUnit.cpp
@@ -2,12 +2,48 @@
 
 SurfboardMainUnit::SurfboardMainUnit(int buttonPin): logger(logger){
     logger = Logger();
-    controller = Controller(logger, buttonPin);
+    syncManager = ControlUnitSyncManager(logger);
+    timeHandler = RTCTimeHandler(logger);
+    statusLighthandler = RGBStatusHandler(logger);
+    buttonHandler = ButtonHandler(logger, buttonPin);
+    status = SystemStatus::SYSTEM_STARTING;
     sampler = Sampler(logger);
 }
+
+
 void SurfboardMainUnit::init(uint8_t samplingUnitsAdresses[][6], int samplingUnitsNum, int RGBRedPin, int RGBGreenPin, int RGBBluePin) {
-    logger.init();
-    controller.init(samplingUnitsAdresses, samplingUnitsNum, RGBRedPin, RGBGreenPin, RGBBluePin);
+
+    status = SystemStatus::SYSTEM_INITIALIZING;
+    try{
+        logger.init();
+        syncManager.init(samplingUnitsAdresses, samplingUnitsNum);
+        statusLighthandler.init(); // todo: update so it receives a light....
+        timeHandler.init();
+        buttonHandler.init();
+
+        // add external sampling units
+        for(int i=0;i<samplingUnitsNum;i++){
+            SamplingUnitRep samplingUnit;
+            memcpy(samplingUnit.mac, samplingUnitsAdresses[i], 6);
+            samplingUnit.status = SamplerStatus::UNIT_STAND_BY;
+            string macString = macToString(samplingUnit.mac);
+            samplingUnits[macString] = samplingUnit;
+        }
+
+        // add internal sampling unit
+        SamplingUnitRep samplingUnit;
+        samplingUnit.mac = INTERNAL_SAMPLING_MAC;
+        samplingUnit.status = SamplerStatus::UNIT_STAND_BY;
+        string macString = macToString(samplingUnit.mac);
+        samplingUnits[macString] = samplingUnit;
+
+        status = SystemStatus::SYSTEM_STAND_BY;
+        logger.info("System initalization complete!");
+        statusLighthandler.updateStatus(status);
+    }catch(exception& e){
+        status = SystemStatus::SYSTEM_ERROR;
+        statusLighthandler.updateStatus(status);
+    }
 }
 void SurfboardMainUnit::addSensor(SensorBase sensor) {
     try{
@@ -15,5 +51,98 @@ void SurfboardMainUnit::addSensor(SensorBase sensor) {
     }catch(InitError& err){
         logger.error("Failed to add sensor");
         // todo: report error status here...
+    }
+}
+void SurfboardMainUnit::startSampling() {
+    if(status == SystemStatus::SYSTEM_STAND_BY){
+        map<string,string> params;
+        int timestamp = timeHandler.getCurrentTimestamp();
+        params[TIMESTAMP] = timestamp;
+        params[IMU_RATE] = 100; // todo: change???
+        try{
+            syncManager.broadcastCommand(ControlUnitCommand::START_SAMPLING, params);
+            sampler.startSampling(timestamp, IMU_RATE);
+        }catch(ESPNowSyncError& error){
+            logger.error("Failed to send command to sampling units!");
+            return;
+        }
+        status = SystemStatus::SYSTEM_SAMPLING;
+        statusLighthandler.updateStatus(status);
+        logger.info("Sampling started...");
+    }
+}
+void SurfboardMainUnit::stopSampling() {
+    if(status == SystemStatus::SYSTEM_SAMPLING){
+        try{
+            map<string,string> params;
+            syncManager.broadcastCommand(ControlUnitCommand::STOP_SAMPLING, params);
+            sampler.stopSampling();
+        }catch(ESPNowSyncError& error){
+            logger.error("Failed to send command to sampling units!");
+            return;
+        }
+        status = SystemStatus::SYSTEM_STAND_BY;        
+        statusLightManager.updateStatus(status);
+    }
+}
+void SurfboardMainUnit::updateSystem() {
+    // status update
+    while(syncManager.hasStatusUpdateMessages()){
+        StatusUpdateMessage statusMessage = syncManager.popStatusUpdateMessage();
+        string unitID = macToString(statusMessage.from);
+        try{
+            SamplingUnitRep& samplingUnit = samplingUnits.at(unitID);
+            samplingUnit.status = statusMessage.status;
+        }
+        catch(const std::out_of_range& ex){
+            logger.error("Status update message received from unknown unit " + unitID);
+        }
+    };
+
+    // internal unit status update
+    string unitID = macToString(INTERNAL_SAMPLING_MAC);
+    try{
+        SamplingUnitRep& samplingUnit = samplingUnits.at(unitID);
+        samplingUnit.status = sampler.getStatus();
+    }
+    catch(const std::out_of_range& ex){
+        logger.error("Status update message received from unknown unit " + unitID);
+    }
+
+    if(buttonManager.wasPressed()){
+        switch(status){
+            case SystemStatus::SYSTEM_SAMPLING:{
+                stopSampling();
+                return;
+            }
+            case SystemStatus::SYSTEM_STAND_BY:{
+                startSampling();
+                return;
+            }
+            default:{
+                // ignore the press
+                logger.info("Pressed button was ignored because system is not in a ready state");
+            }
+        }
+    }
+
+    std::map<string, SamplingUnitRep>::iterator it = samplingUnits.begin();
+    while (it != samplingUnits.end()) {
+        if(it->second.mac == INTERNAL_SAMPLING_MAC){
+            continue;
+        }else if(status == SystemStatus::SYSTEM_SAMPLING && it->second.status != SamplerStatus::UNIT_SAMPLING){
+            try{
+                espSyncManager.sendCommand(ControlUnitCommand::START_SAMPLING, it->second.mac);
+            }catch(ESPNowSyncError& error){
+                it->second.status = SamplerStatus::UNIT_ERROR;
+            }  
+        }else if(status != SystemStatus::SYSTEM_SAMPLING && it->second.status == SamplerStatus::UNIT_SAMPLING){
+            try{
+                espSyncManager.sendCommand(ControlUnitCommand::STOP_SAMPLING, it->second.mac);
+            }catch(ESPNowSyncError& error){
+                it->second.status = SamplerStatus::UNIT_ERROR;
+            }    
+        }
+        it++;
     }
 };
--- src/SurfboardMainUnit/SurfboardMainUnit.h
+++ src/SurfboardMainUnit/SurfboardMainUnit.h
@@ -6,16 +6,36 @@
 using namespace std;
 
 #include "../Sampler/Sampler.h"
-#include "../Controller.h"
+#include "Sync/ControlUnitSyncManager.h"
+#include "../Utils/Logger.h"
+#include "../Utils/Status.h"
+#include "../Components/IO/RTCTimeHandler.h"
+#include "../Components/IO/RGBStatusHandler.h"
+#include "../Components/IO/ButtonHandler.h"
+
+#include "../Status/RGBStatusManager.h"
+#include "../IO/SamplingButtonManager.h"
 
 // todo: merge Controller to this class and remove Controller...
 // add is_internal to SamplingUnitRep
 
+typedef struct SamplingUnitRep{
+    uint8_t mac[6];
+    SamplerStatus status;    
+} SamplingUnitRep;
+
+const uint8_t[6] INTERNAL_SAMPLING_MAC = {0x0,0x0,0x0,0x0,0x0,0x0};
+
 class SurfboardMainUnit {
     private:
-        Sampler sampler;
-        Controller controller;
+        std::map<string, SamplingUnitRep> samplingUnits; // mac string to instance mapping
+        ControlUnitSyncManager syncManager;
+        RTCTimeHandler timeHandler;
+        RGBStatusHandler statusLighthandler;
+        ButtonHandler buttonHandler;
         Logger logger;
+        Sampler sampler;
+        SystemStatus status;
     public:
         SurfboardMainUnit(){};
         SurfboardMainUnit(int buttonPin);
@@ -23,6 +43,11 @@ class SurfboardMainUnit {
         void init(uint8_t samplingUnitsAdresses[][6], int samplingUnitsNum, int RGBRedPin, int RGBGreenPin, int RGBBluePin);
 
         void addSensor(SensorBase sensor);
+
+        void startSampling();
+        void stopSampling();
+
+        void updateSystem();
 
 };
 
