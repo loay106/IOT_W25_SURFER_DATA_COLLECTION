6c301592560dff702dd817d5d68dd173bb605a47
Loay Khateeb <loay.106@gmail.com>
2025-02-05 10:58:13 +0200

    update sampling unit wifi handling in setup phase + add demo sensors addings

--- SurfboardSamplingUnit/SurfboardSamplingUnit.ino
+++ SurfboardSamplingUnit/SurfboardSamplingUnit.ino
@@ -1,7 +1,7 @@
 #include "SurfboardSamplingUnit.h"
 
 // constants
-int serialBaudRate = 57600;
+int serialBaudRate = 115200;
 int SDCardChipSelectPin = 5;
 uint8_t CONTROL_UNIT_MAC[6] = {0xA8, 0x42, 0xE3, 0x46, 0xE0, 0x64};
 
@@ -40,18 +40,21 @@ void setup() {
 
 
     WifiHandler* wifiHandler = new WifiHandler(WIFI_SSID, WIFI_PASSWORD);
-    try{
-        logger->info("Checking wifi connection...");
-        wifiHandler->init();
-        wifiHandler->connect();
-        logger->info("Wifi connection established!");
-        WIFI_ESP_NOW_CHANNEL = wifiHandler->getChannel();
-        logger->info("Setting ESP Now channel to " + to_string(WIFI_ESP_NOW_CHANNEL));
-        wifiHandler->disconnect();
-        logger->info("Wifi disconnected!");
-    }catch(...){
-          logger->error("Wifi connection failed! Check your ssid and password!");
-          while(true){delay(500);};
+    while(true){
+        try{
+            logger->info("Checking wifi connection...");
+            wifiHandler->init();
+            wifiHandler->connect();
+            logger->info("Wifi connection established!");
+            WIFI_ESP_NOW_CHANNEL = wifiHandler->getChannel();
+            logger->info("Setting ESP now channel to " + to_string(WIFI_ESP_NOW_CHANNEL));
+            wifiHandler->disconnect();
+            logger->info("Wifi disconnected!");
+            break;
+        }catch(...){
+            logger->error("Wifi connection failed! Please check your wifi ssid and password!");
+            delay(3000);
+        }
     }
 
     CloudSyncManager* cloudSyncManager = new CloudSyncManager(logger, wifiHandler, wifiHandler->getMacAddress());
@@ -61,37 +64,37 @@ void setup() {
     samplingUnit = new SurfboardSamplingUnit(syncManager,sdCardHandler,sampler, logger);
 
     // declare sensors here....
-    Force_HX711* sensor0 = new Force_HX711(logger,sdCardHandler,stoi(sensorsParams[0]) ,doutPin,sckPin);
-    IMU_BNO080* sensor1 = new IMU_BNO080(logger, sdCardHandler,stoi(sensorsParams[1]));
-    //Force_FAKE* sensor2 = new Force_FAKE(logger,sdCardHandler);
-    //Force_FAKE* sensor3 = new Force_FAKE(logger,sdCardHandler);
+    //Force_HX711* sensor0 = new Force_HX711(logger,sdCardHandler,stoi(sensorsParams[0]) ,doutPin,sckPin);
+    //IMU_BNO080* sensor1 = new IMU_BNO080(logger, sdCardHandler,stoi(sensorsParams[1]));
+    Mock_HX711* mock_force_2 = new Mock_HX711(logger,sdCardHandler, 1000);
+    Mock_HX711* mock_force_3 = new Mock_HX711(logger,sdCardHandler, 1000);
+    Mock_HX711* mock_force_4 = new Mock_HX711(logger,sdCardHandler, 1000);
 
     try{
         // don't change the order of the init
         logger->init(serialBaudRate);
         syncManager->init(CONTROL_UNIT_MAC, WIFI_ESP_NOW_CHANNEL);
         sampler->init();
-        sensor0->init();
-        sensor1->init();
-        //sensor2->init();
-        //sensor3->init();
         // init sensors here..
-        // you can pass params from the config file
-        // sensor[i] should have the param in sensorsParams[i]
+       // sensor0->init();
+        //sensor1->init();
+        mock_force_2->init();
+        mock_force_3->init();
+        mock_force_4->init();
     }catch(InitError& err){
         while(true){
             // don't proceed, there's a wiring error!
             logger->error("Init error! Check your wiring!");
             delay(100);
         }
-        
     }
 
     // add sensors here....
-    samplingUnit->addSensor(sensor0);
-    samplingUnit->addSensor(sensor1);
-    //samplingUnit->addSensor(sensor2);
-    //samplingUnit->addSensor(sensor3);
+   // samplingUnit->addSensor(sensor0);
+   // samplingUnit->addSensor(sensor1);
+    samplingUnit->addSensor(mock_force_2);
+    samplingUnit->addSensor(mock_force_3);
+    samplingUnit->addSensor(mock_force_4);
 
     try{
         syncManager->connect();
